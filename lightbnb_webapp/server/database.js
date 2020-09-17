b097ec969cc730c7b6b80df835df3be648ce37f0
Fatima Nguyen <fatima3455@hotmail.com>
2020-09-17 12:53:42 +0000

    add getallreservation function

--- lightbnb_webapp/server/database.js
+++ lightbnb_webapp/server/database.js
@@ -71,11 +71,7 @@ const addUser =  function(user) {
     values:[user.name, user.email, user.password],
   }
   return pool.query(query)
-            .then(res => res.rows[0])
-  // / const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-  // return Promise.resolve(user);
+            .then(res => res.rows[0]);
 }
 exports.addUser = addUser;
 
@@ -87,7 +83,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = {
+    text: `SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id 
+    WHERE reservations.guest_id = $1
+    AND reservations.end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT 10;`,
+    values: [guest_id]
+  }
+  return pool.query(query)
+      .then (res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
